87b1e1159a5d2f3c786fe15668dbb5520a3aec3f
pradumya004 <pradumya004@gmail.com>
2025-02-04 08:43:19 +0530

    Update LoginPage.jsx

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -24,7 +24,7 @@ const LoginPage = () => {
     e.preventDefault();
   
     try {
-      const response = await fetch("http://localhost:3000/api/auth/login", {
+      const response = await fetch("https://getmaxcrm.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
